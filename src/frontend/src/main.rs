582a8a8fbd37a748358ee87006c73dec2c48aa86
Cédric Cabessa <ced@ryick.net>
2023-11-11 15:50:28 +0100

    front: use component

--- src/frontend/src/main.rs
+++ src/frontend/src/main.rs
@@ -1,40 +1,55 @@
 use gloo_net::http::Request;
+use gloo_net::Error;
 use yew::prelude::*;
 
 use libriirview::json::Category;
+
+#[derive(Properties, PartialEq)]
+struct CategoriesProps {
+    categories: Vec<Category>,
+}
 
 #[function_component(PleaseWait)]
 fn please_wait() -> Html {
     html! {<div class="content-area">{"WAIT"}</div>}
 }
 
+async fn fetch_category(url: &str) -> Result<Vec<Category>, Error> {
+    let categories = Request::get(url).send().await?.json().await?;
+    Ok(categories)
+}
+
+#[function_component(Categories)]
+fn component_category(CategoriesProps { categories }: &CategoriesProps) -> Html {
+    categories
+        .iter()
+        .map(|cat| {
+            html! {
+            <div key={cat.uid.clone()}>{cat.name.clone()}</div>
+            }
+        })
+        .collect()
+}
+
 #[function_component(AppContent)]
 fn app_content() -> HtmlResult {
-    let categories = use_state(|| vec![]);
+    let categories = use_state(|| Ok(vec![]));
     {
         let categories = categories.clone();
         use_effect_with((), move |_| {
             wasm_bindgen_futures::spawn_local(async move {
-                let fetch_categories: Vec<Category> =
-                    Request::get("http://localhost:8000/categories")
-                        .send()
-                        .await
-                        .unwrap()
-                        .json()
-                        .await
-                        .unwrap();
-                categories.set(fetch_categories);
+                match fetch_category("http://localhost:8000/categories").await {
+                    Ok(fetched_categories) => categories.set(Ok(fetched_categories)),
+                    Err(e) => categories.set(Err(e)),
+                };
             });
-            || ()
         });
     }
 
-    let html = (*categories)
-        .iter()
-        .map(|c| {
-            html! {<p>{c.name.clone()}</p>}
-        })
-        .collect();
+    let html = match &(*categories) {
+        Ok(state) => html! {<Categories categories={(*state).clone()} />},
+        Err(e) => html! {<p>{e.to_string()}</p>},
+    };
     Ok(html)
 }
 
--- src/libriirview/src/json.rs
+++ src/libriirview/src/json.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, PartialEq)]
 pub struct Category {
     pub uid: String,
     pub name: String,
