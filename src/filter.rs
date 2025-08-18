9c052d4584ae3f2a95398fad665e10ceb118e483
Cédric Cabessa <ced@ryick.net>
2025-08-18 10:20:52 +0200

    wip: add test for repo & state parser

--- src/filter.rs
+++ src/filter.rs
@@ -4,12 +4,16 @@ use anyhow::Result;
 pub struct Filter {
     pub title: String,
     pub author: String,
+    pub repo: String,
+    pub state: String,
 }
 
 impl Filter {
     pub fn parse(input: &str) -> Result<Self> {
         let mut title = String::new();
         let mut author = String::new();
+        let mut repo = String::new();
+        let mut state = String::new();
 
         let known_keys = ["title:", "author:"];
         let mut key_positions: Vec<(&str, usize)> = known_keys
@@ -49,7 +53,12 @@ impl Filter {
             }
         }
 
-        Ok(Filter { title, author })
+        Ok(Filter {
+            title,
+            author,
+            repo,
+            state,
+        })
     }
 }
 
@@ -62,17 +71,62 @@ mod test {
         let filters = Filter::parse("title:Rust Programming author:John Doe").unwrap();
         assert_eq!(filters.title, "Rust Programming");
         assert_eq!(filters.author, "John Doe");
+        assert!(filters.repo.is_empty());
+        assert!(filters.state.is_empty());
 
         let filters = Filter::parse("author:John Doe").unwrap();
         assert!(filters.title.is_empty());
         assert_eq!(filters.author, "John Doe");
+        assert!(filters.repo.is_empty());
+        assert!(filters.state.is_empty());
 
         let filters = Filter::parse("Rust Programming").unwrap();
         assert_eq!(filters.title, "Rust Programming");
         assert!(filters.author.is_empty());
+        assert!(filters.repo.is_empty());
+        assert!(filters.state.is_empty());
 
         let filters = Filter::parse("Rust Programming author:John Doe").unwrap();
         assert_eq!(filters.title, "Rust Programming");
         assert_eq!(filters.author, "John Doe");
+        assert!(filters.repo.is_empty());
+        assert!(filters.state.is_empty());
+
+        let filters =
+            Filter::parse("Rust Programming author:John Doe repo:LedgerHQ/ledger-live").unwrap();
+        assert_eq!(filters.title, "Rust Programming");
+        assert_eq!(filters.author, "John Doe");
+        assert_eq!(filters.repo, "LedgerHQ/ledger-live");
+        assert!(filters.state.is_empty());
+
+        let filters = Filter::parse("title:Rust Programming state:open").unwrap();
+        assert_eq!(filters.title, "Rust Programming");
+        assert!(filters.author.is_empty());
+        assert!(filters.repo.is_empty());
+        assert_eq!(filters.state, "Open");
+
+        let filters = Filter::parse("title:Rust Programming state:Open").unwrap();
+        assert_eq!(filters.title, "Rust Programming");
+        assert!(filters.author.is_empty());
+        assert!(filters.repo.is_empty());
+        assert_eq!(filters.state, "Open");
+
+        let filters = Filter::parse("title:Rust Programming state:Resolved").unwrap();
+        assert_eq!(filters.title, "Rust Programming");
+        assert!(filters.author.is_empty());
+        assert!(filters.repo.is_empty());
+        assert_eq!(filters.state, "Resolved");
+
+        let filters = Filter::parse("title:Rust Programming state:Resolved").unwrap();
+        assert_eq!(filters.title, "Rust Programming");
+        assert!(filters.author.is_empty());
+        assert!(filters.repo.is_empty());
+        assert_eq!(filters.state, "Resolved");
+
+        let filters = Filter::parse("title:Rust Programming state:Pasfini").unwrap();
+        assert_eq!(filters.title, "Rust Programming");
+        assert!(filters.author.is_empty());
+        assert!(filters.repo.is_empty());
+        assert!(filters.state.is_empty());
     }
 }
